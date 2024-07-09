b14a030b51d85108c325e5b406b4a747a4ac28b0
Dayo <dayo05@daum.net>
2024-07-09 20:17:47 +0900

    Implement open application function

---
+++ app/src/main/java/com/teammaso/lcs/functions/OpenApplication.kt
@@ -0,0 +1,24 @@
+package com.teammaso.lcs.functions
+
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import com.teammaso.lcs.LuaService
+import com.teammaso.lcs.ap.LuaFunction
+
+object OpenApplication {
+    @LuaFunction(name = "open_app")
+    fun openApp(pkg: String) {
+        val service = LuaService.INSTANCE!!
+        val li = service.packageManager?.getLaunchIntentForPackage(pkg)
+        li?.let {
+            service.startActivity(it)
+        } ?: Log.w("OpenApplication", "Application $pkg not found")
+    }
+
+    @LuaFunction(name = "open_url")
+    fun openUrl(url: String) {
+        val service = LuaService.INSTANCE!!
+        service.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+    }
+}
