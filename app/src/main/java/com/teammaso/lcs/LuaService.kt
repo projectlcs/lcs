444b002942140b170b813cd21f513e601e6de451
seojinhyeong99 <sjh040514@kookmin.ac.kr>
2024-07-12 12:04:02 +0900

    Add create_Alarm

--- app/src/main/java/com/teammaso/lcs/LuaService.kt
+++ app/src/main/java/com/teammaso/lcs/LuaService.kt
@@ -34,7 +34,8 @@ class LuaService: Service() {
 
         INSTANCE = this
 
-        lua.load("set_do_not_disturb(not get_do_not_disturb())")
+        //lua.load("set_do_not_disturb(not get_do_not_disturb())")
+        lua.load("create_Alarm(12,5)")
         lua.pCall(0,0)
     }
 
