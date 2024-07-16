b1ffb305f16fdcffe0ba28a6d3855af30ce52e8d
Dayo <dayo05@daum.net>
2024-07-17 00:41:48 +0900

    add logger lua function

---
+++ app/src/main/java/net/projectlcs/lcs/functions/Logger.kt
@@ -0,0 +1,14 @@
+package net.projectlcs.lcs.functions
+
+import android.util.Log
+import net.projectlcs.lcs.ap.LuaFunction
+import net.projectlcs.lcs.ap.LuaProvider
+
+@LuaProvider
+object Logger {
+    @LuaFunction(name = "debug_log")
+    fun printDebug(message: String) = Log.d("Lua", message)
+
+    @LuaFunction(name = "debug_log")
+    fun printDebug(tag: String, message: String) = Log.d(tag, message)
+}
