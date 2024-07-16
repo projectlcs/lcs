cdf65441e2b534475c904352ea9a55c5496afeed
Dayo <dayo05@daum.net>
2024-07-16 22:44:49 +0900

    add warning comment

--- app/src/main/java/net/projectlcs/lcs/functions/PermissionProvider.kt
+++ app/src/main/java/net/projectlcs/lcs/functions/PermissionProvider.kt
@@ -1,5 +1,7 @@
 package net.projectlcs.lcs.functions
 
+// DO NOT RENAME OR MOVE PACKAGE OF THIS INTERFACE.
+// THIS IS USED INTERNALLY AT ANNOTATION PROCESSOR.
 interface PermissionProvider {
     /**
      * @param tryRequest true if to open permission request window
