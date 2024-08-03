6806cc04a577d7700186daa3b14b0c6954c522ae
Dayo <dayo05@daum.net>
2024-08-03 20:45:07 +0900

    rewrite PermissionProvider

--- app/src/main/java/net/projectlcs/lcs/functions/PermissionProvider.kt
+++ app/src/main/java/net/projectlcs/lcs/functions/PermissionProvider.kt
@@ -1,12 +1,39 @@
 package net.projectlcs.lcs.functions
 
-// DO NOT RENAME OR MOVE PACKAGE OF THIS INTERFACE.
-// THIS IS USED INTERNALLY AT ANNOTATION PROCESSOR.
-interface PermissionProvider {
-    /**
-     * @param tryRequest true if to open permission request window
-     *
-     * TODO: rewrite using coroutine
-     */
-    fun verifyPermission(tryRequest: Boolean): Boolean = true
+import android.widget.Toast
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import me.ddayo.aris.CoroutineProvider
+import me.ddayo.aris.CoroutineProvider.CoroutineReturn
+import net.projectlcs.lcs.LuaService
+
+interface PermissionProvider: CoroutineProvider {
+    fun verifyPermission(): Boolean
+    fun requestPermission()
+
+    suspend fun<T> SequenceScope<CoroutineReturn<T>>.requestPermission(then: suspend SequenceScope<CoroutineReturn<T>>.() -> Unit) {
+        val beginTime = System.currentTimeMillis()
+        var isTimeout = false
+
+        if(!verifyPermission()) {
+            requestPermission()
+            yieldUntil {
+                if (System.currentTimeMillis() - beginTime > 60000) {
+                    GlobalScope.launch(Dispatchers.Main) {
+                        Toast.makeText(
+                            LuaService.INSTANCE!!.applicationContext,
+                            "Permission request timeout!!",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                    isTimeout = true
+                    true
+                } else verifyPermission()
+            }
+        }
+
+        if(!isTimeout)
+            then()
+    }
 }
