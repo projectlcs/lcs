c3e94c0f86a0f57c59cdd0fad97f1a58d85ac687
brainVRG <woo95822@naver.com>
2024-11-14 09:30:16 +0900

    Prompt 함수 변경

--- app/src/main/java/net/projectlcs/lcs/ai/PromptEngineering.kt
+++ app/src/main/java/net/projectlcs/lcs/ai/PromptEngineering.kt
@@ -70,6 +70,31 @@ end
     return sel_fn(...)
 end
 
+function show_yes_no_dialog(...)
+    local as_table = { ... }
+    local table_size = #as_table
+    local score = -1
+    local sel_fn = function() error("No matching argument") end
+if table_size >= 2 then
+    local task_score = 0
+    if task_score >= score then
+        score = task_score
+        sel_fn = function(...)
+local coroutine = show_yes_no_dialog_kt0(...) -- get LuaCoroutine instance
+while true do
+    local it = coroutine:next_iter()
+    if it:is_break() then
+        return it:value()
+    end
+    task_yield(function() return it:finished() end)
+end
+        end
+    end
+end
+
+    return sel_fn(...)
+end
+
 function create_file(...)
     local as_table = { ... }
     local table_size = #as_table
@@ -500,6 +525,31 @@ end
     return sel_fn(...)
 end
 
+function SubwayApiTest(...)
+    local as_table = { ... }
+    local table_size = #as_table
+    local score = -1
+    local sel_fn = function() error("No matching argument") end
+if table_size >= 0 then
+    local task_score = 0
+    if task_score >= score then
+        score = task_score
+        sel_fn = function(...)
+local coroutine = SubwayApiTest_kt0(...) -- get LuaCoroutine instance
+while true do
+    local it = coroutine:next_iter()
+    if it:is_break() then
+        return it:value()
+    end
+    task_yield(function() return it:finished() end)
+end
+        end
+    end
+end
+
+    return sel_fn(...)
+end
+
 function get_time(...)
     local as_table = { ... }
     local table_size = #as_table
@@ -522,106 +572,90 @@ end
 
 함수 사용 예시:
 ## create_alarm(hour: Int, minutes: Int)
-```
  createAlarm(15, 12) // set new alarm at 3:12 PM
 
  @param hour time of hour to set alarm
  @param minutes time of minutes to set alarm
-```
+
 
 
 ## set_do_not_disturb(newValue: Boolean)
-```
  @param newValue new do not disturb state
-```
+
 
 
 ## get_do_not_disturb()
-```
  @return current do not disturb status
-```
 
 
 ## create_file(name: String)
-```
  Create file inside application-data directory
  @param name filename to create
-```
+
 
 
 ## delete_file(name: String)
-```
  Delete file inside application-data directory
  @param name filename to delete
-```
+
 
 
 ## write_file(name: String, text: String)
-```
  Write(overwrite) to file inside application-data directory
  @param name filename to write
  @param text text to write
-```
+
 
 
 ## append_file(name: String, text: String)
-```
  Write(append) to file inside application-data directory
  @param name filename to append
  @param text text to write
-```
+
 
 
 ## read_file(name: String)
-```
  Read the file inside application-data directory
  @param name name of file to read
  @return content of given file
-```
+
 
 
 ## create_file_global(name: String)
-```
  Create file inside global(external) storage. This requires MANAGE_EXTERNAL_STORAGE permission
  @param name filename to create
-```
+
 
 
 ## delete_file_global(name: String)
-```
  Delete file inside global(external) storage. This requires MANAGE_EXTERNAL_STORAGE permission
  @param name filename to create
-```
 
 
 ## write_file_global(name: String, text: String)
-```
  Write(overwrite) file inside global(external) storage. This requires MANAGE_EXTERNAL_STORAGE permission
  @param name filename to create
  @param text text to write
-```
+
 
 
 ## append_file_global(name: String, text: String)
-```
  Write(append) file inside global(external) storage. This requires MANAGE_EXTERNAL_STORAGE permission
  @param name filename to create
  @param text text to write
-```
+
 
 
 ## read_file_global(name: String)
-```
  Read file inside global(external) storage. This requires MANAGE_EXTERNAL_STORAGE permission
  @param name filename to create
  @return content of given file
-```
+
 
 
 ## get_location()
-```
  @return This function returns three values: latitude, longitude, accuracy
-```
+
 
 
 ## debug_log(message: String)
@@ -629,79 +663,67 @@ end
 
 
 ## send_web_request(url: String)
-```
  Send HTTP Get request then retrieve response code and its data
 
  @param url target url
  @return response_code: Int, response_data: String
-```
+
 
 
 ## download_file(url: String, name: String)
-```
  Download file via HTTP Get request at provided name.
  If body is empty then it just creates empty file
 
  @param url target url
  @param name file name to save
  @return response_code: Int
-```
 
 
 ## send_notification(task: AndroidLuaTask, message: String)
-```
  Send notification with existing task
 
  @param task the LuaTask. just use `task` which already passed to function.
  @param message message to send
-```
+
 ## send_notification(task: AndroidLuaTask, title: String, text: String)
-```
  Send notification with provided title and text
 
  @param task the LuaTask. just use `task` which already passed to function.
  @param title title of notification
  @param text inner text of notification
-```
 
 
 ## send_long_toast(text: String)
-```
  Send long toast message
 
  @param text text to displayed by long toast message
-```
+
 
 
 ## send_toast(text: String)
-```
- Send toast message
+Send toast message
 
  @param text text to displayed by toast message
-```
+
 
 
 ## open_app(pkg: String)
-```
  Open application by package name
 
  @param pkg Application package name
-```
+
 
 
 ## open_url(url: String)
-```
  Open webpage by default browser
 
  @param url url to open
-```
+
 
 
 ## get_time()
-```
  System.currentTimeMillis()
 
  @return current datetime from 1/1/1970
-```
     """.trimIndent()
 }
